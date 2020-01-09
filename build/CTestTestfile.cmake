f60308fe4fedd7df6b7b37201feaee0fa6f0eb62
kaenguru123 <daskaenguru1234@gmail.com>
2020-01-09 07:37:26 -0800

    Add Jenkinsfile

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julian/Workspace/repos/Calc
-# Build directory: /home/julian/Workspace/repos/Calc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runUnitTests "test/test.cpp")
