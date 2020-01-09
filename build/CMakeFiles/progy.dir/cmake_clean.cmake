c9c47c4b11666f60806d7c843d670c8fce27317f
kaenguru123 <daskaenguru1234@gmail.com>
2020-01-09 07:35:00 -0800

    Add Jenkinsfile

---
+++ build/CMakeFiles/progy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/progy.dir/main.cpp.o"
+  "progy.pdb"
+  "progy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/progy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
